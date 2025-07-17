686b8575ae6e2574c9f25f8e4cb06de82b42ef47
eitay <alter1eitai@gmail.com>
2025-07-17 17:23:57 +0300

    adding basic code

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
 const Switcher = require('switcher-js2');
 const envFile = require('./env.json');
-//
+const mqtt = require('mqtt')
+
 const switcher = new Switcher(envFile.DEVICE_ID, envFile.DEVICE_IP,console.log, true, envFile.DEVICE_TYPE, false, envFile.API_KEY, envFile.DEVICE_KEY);
-
-//
-// switcher.get_state().then((state) => {
-//     console.log('📋 Device State:', state);
-// }).catch((err) => {
-//     console.error('⚠️ Error getting device state:', err);
-// });
-
-switcher.turn_on()
-
-// async function status(switcher) {
-//     const s = await switcher.status()
-//     console.log(s)
-// }
-//
-// status(switcher)
-//
-//
-// switcher.on('status', s => {
-//     console.log(s)
-// })
-
-switcher.close()
 
 const proxy = Switcher.listen(console.log);
 
 proxy.on('message', (message) => {
-    console.log(message)
+    const msg = {
+        name: message.name,
+        id: message.device_ip,
+        is_on: Boolean(message.state.power)
+    }
+    console.log(msg)
 });
 
-proxy.close();
+switcher.close()
---
+++ ultra_sonic.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO setup
+TRIG = 17
+ECHO = 27
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+def get_distance():
+    # Send trigger pulse
+    GPIO.output(TRIG, False)
+    time.sleep(0.0002)
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)  # 10µs pulse
+    GPIO.output(TRIG, False)
+
+    # Wait for echo start
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+
+    # Wait for echo end
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    distance_cm = pulse_duration * 17150  # speed of sound
+
+    return round(distance_cm, 2)
+
+try:
+    while True:
+        dist = get_distance()
+        print(f"Distance: {dist} cm")
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
